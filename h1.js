9f0239042793a815b8431f8e93648a92b5a9ea1f
Dhruvik_Pativala <pativaladhruvik76@gmail.com>
2026-08-22 11:17:28 +0530

    soorted array

--- h1.js
+++ h1.js
@@ -1,1 +1,8 @@
-const h ={"name":"lju"}
+const h =[{name:"lju",n1:30},{name:"l2",n1:11}]
+const time = new Date()
+console.log(time)
+const sort = h.sort((b,a)=>b.n1-a.n1)[0]
+console.log(sort)
+for (x of h){
+    console.log(x.name +" "+x.n1)
+}
